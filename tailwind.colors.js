bcf362ef3cc629a311317a39c792e6aa9e7c30d6
sugeetsharma <68314861+sugeetsharma@users.noreply.github.com>
2024-09-20 18:05:06 +0530

    Initial tailwind setup

---
+++ tailwind.colors.js
@@ -0,0 +1,62 @@
+module.exports = {
+  transparent: "transparent",
+  current: "currentColor",
+  inherit: "inherit",
+  white: "#ffffff",
+  black: "#000000",
+  grey: {
+    50: "#F5F5F5",
+    100: "#ECECEC",
+    200: "#C6C6C6",
+    300: "#A1A1A1",
+    400: "#8A8A8A",
+    500: "#707070",
+    600: "#5B5B5B",
+    700: "#434343",
+    800: "#2C2C2C",
+    900: "#141414",
+    light: "#FAFAFA"
+  },
+  
+  blue: {
+    // to use gradient add classes "bg-gradient-to-r from-blue-mid to-blue-dark"
+    50: "#DFECFF",
+    100: "#BED9FF",
+    200: "#9EC6FF",
+    300: "#7DB3FF",
+    400: "#5DA0FF",
+    500: "#3C8DFF",
+    600: "#1C7AFF",
+    700: "#0068FA",
+    800: "#005BDB",
+    900: "#004EBC",
+    light: "#0068FA",
+    mid: "#005BDB",
+    dark: "#004EBC",
+  },
+  validation: {
+    red: "#CE3939",
+    green: "#30855D",
+  },
+  green: {
+    100: "#6CBA33",
+  },
+  success: {
+    50: "#E8F6EF",
+    700: "#30855D",
+    800: "#296548",
+    900: "#294739",
+  },
+  error: {
+    50: "#FEECEF",
+    700: "#CE3939",
+    800: "#C13332",
+    900: "#B12A28",
+  },
+  warning: {
+    50: "#FDF2E0",
+    700: "#E98614",
+    800: "#E37712",
+    900: "#DC680F",
+  },
+};
