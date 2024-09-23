8af42b3b4c8ed67d2361a4ebd23f186f51eb9418
sugeetsharma <68314861+sugeetsharma@users.noreply.github.com>
2024-09-23 14:21:47 +0530

    done

--- tab/tab.js
+++
@@ -1,4 +0,0 @@
-const div = document.createElement('div');
-  div.classList.add('tw');
-
-alert("this is tabs block component");
---
+++ tabs/tabs.js
@@ -0,0 +1,23 @@
+export default function decorate(block) {
+	
+	
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
+	
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+		alert ("tabs component2");
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+    });
+  });
+}
+
+alert ("tabs component1");
